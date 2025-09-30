4a0153ce6b4aea43c54dee0a3dd4ca03c3f3b21d
Akeka_ <vladvaledol@gmail.com>
2025-09-30 21:14:52 +0300

    Docker-Compose

--- backend/src/main.py
+++ backend/src/main.py
@@ -26,4 +26,4 @@ app.include_router(ws_router)
 app.include_router(auth_router)
 
 if __name__ == '__main__':
-    uvicorn.run('main:app', reload=True, host='localhost', port=8000)
+    uvicorn.run('main:app', reload=True, host='0.0.0.0', port=8000)
